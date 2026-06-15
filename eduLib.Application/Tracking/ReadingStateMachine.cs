db8f7efc1f2434cdd6ae3db0a701281a60bbbc35
Tegar <tegars444@gmail.com>
2026-06-15 10:48:01 +0700

    Menambahkan pesan di setiap method

--- eduLib.Application/Tracking/ReadingStateMachine.cs
+++ eduLib.Application/Tracking/ReadingStateMachine.cs
@@ -3,20 +3,19 @@ using eduLib.Core.Enums;
 
 namespace eduLib.Application.Tracking
 {
-    // Teknik 1: Automata (State Machine untuk progres bacaan)
     public class ReadingStateMachine
     {
         public ReadingState CurrentState { get; private set; } = ReadingState.NotStarted;
 
         public void UpdateProgress(int currentPage, int totalPages)
         {
-            // Defensive Programming (DbC): Preconditions
+            // defensive Programming 
             if (totalPages <= 0)
                 throw new ArgumentException("Total halaman harus lebih dari 0.");
             if (currentPage < 0 || currentPage > totalPages)
                 throw new ArgumentException("Halaman saat ini tidak valid.");
 
-            // Logika Transisi Automata
+            // logika Transisi Automata
             if (currentPage == 0)
             {
                 CurrentState = ReadingState.NotStarted;
